4f57770dcb9e301c870f2148759dcf27f1184057
Irfan <irfanalamt@gmail.com>
2023-06-22 13:38:12 -0400

    Feat: set up basic chat layout and server-side custom message handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,99 @@
-import {useEffect, useState} from 'react';
-import {io, Socket} from 'socket.io-client';
-const SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL || '';
+import React, {useEffect, useRef, useState} from 'react';
+import io from 'socket.io-client';
 
-const Home = () => {
-  const [socket, setSocket] = useState<Socket>();
+const socket = io(process.env.NEXT_PUBLIC_SERVER_URL || '');
+
+interface Message {
+  type: string;
+  text: string;
+}
+
+const ChatPage: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([{type: '', text: ''}]);
+  const [inputValue, setInputValue] = useState('');
+  const messagesEndRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const newSocket = io(SERVER_URL, {});
-    setSocket(newSocket);
+    socket.on('chat message', (message: string) => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          type: 'server',
+          text: message,
+        },
+      ]);
+    });
 
     return () => {
-      newSocket.close();
+      socket.disconnect();
     };
   }, []);
 
   useEffect(() => {
-    if (!socket) return;
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({behavior: 'smooth'});
+    }
+  }, [messages]);
 
-    socket.on('connect', () => {
-      console.log('Connected to server');
-    });
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
-    socket.on('disconnect', () => {
-      console.log('Disconnected from server');
-    });
-  }, [socket]);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    socket.emit('chat message', inputValue);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        type: 'user',
+        text: inputValue,
+      },
+    ]);
+    setInputValue('');
+  };
 
   return (
-    <div>
-      <h1>socket connection has been established!</h1>
-      <h2>We're officially connected 🎉</h2>
+    <div className='flex flex-col h-screen justify-between items-center bg-gray-200 px-6 py-6'>
+      <h4 className='text-2xl text-center text-gray-800 font-bold mb-4'>
+        RFN Chat
+      </h4>
+      <ul className='flex-grow overflow-auto mb-6 bg-white rounded-lg shadow-inner p-6 max-w-lg w-full'>
+        {messages.map(
+          (message, index) =>
+            message.text && (
+              <li
+                key={index}
+                className={`my-4 p-4 rounded-lg max-w-xs transition-all duration-200 ease-in-out ${
+                  message.type === 'user'
+                    ? 'bg-blue-200 text-gray-700 ml-auto rounded-br-none'
+                    : 'bg-gray-200 text-gray-700 mr-auto rounded-bl-none'
+                } `}>
+                {message.text}
+              </li>
+            )
+        )}
+        <div ref={messagesEndRef} />
+      </ul>
+
+      <form
+        onSubmit={handleSubmit}
+        className='flex items-center justify-between bg-white rounded-lg shadow-md px-6 py-3 max-w-lg w-full'>
+        <input
+          type='text'
+          value={inputValue}
+          onChange={handleInputChange}
+          className='flex-grow bg-gray-200 rounded-md px-4 py-2 mr-4 text-lg focus:outline-none focus:ring-2 focus:ring-indigo-400'
+          placeholder='Type a message'
+        />
+        <button
+          type='submit'
+          className='text-white bg-indigo-600 px-6 py-2 rounded-md hover:bg-indigo-700 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-indigo-400'>
+          Send
+        </button>
+      </form>
     </div>
   );
 };
 
-export default Home;
+export default ChatPage;
