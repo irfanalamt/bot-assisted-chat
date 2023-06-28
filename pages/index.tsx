e254a69880073d6077bb178464c821cfeda956aa
Irfan <irfanalamt@gmail.com>
2023-06-28 10:38:32 -0400

    Feat: design basic index layout; routes for agent and user logins

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,99 +1,34 @@
-import React, {useEffect, useRef, useState} from 'react';
-import io from 'socket.io-client';
+import React from 'react';
+import {useRouter} from 'next/navigation';
 
-const socket = io(process.env.NEXT_PUBLIC_SERVER_URL || '');
-
-interface Message {
-  type: string;
-  text: string;
-}
-
-const ChatPage: React.FC = () => {
-  const [messages, setMessages] = useState<Message[]>([{type: '', text: ''}]);
-  const [inputValue, setInputValue] = useState('');
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    socket.on('chat message', (message: string) => {
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        {
-          type: 'server',
-          text: message,
-        },
-      ]);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({behavior: 'smooth'});
-    }
-  }, [messages]);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-
-    socket.emit('chat message', inputValue);
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      {
-        type: 'user',
-        text: inputValue,
-      },
-    ]);
-    setInputValue('');
-  };
-
+const Index: React.FC = () => {
+  const router = useRouter();
   return (
-    <div className='flex flex-col h-screen justify-between items-center bg-gray-200 px-6 py-6'>
-      <h4 className='text-2xl text-center text-gray-800 font-bold mb-4'>
-        RFN Chat
-      </h4>
-      <ul className='flex-grow overflow-auto mb-6 bg-white rounded-lg shadow-inner p-6 max-w-lg w-full'>
-        {messages.map(
-          (message, index) =>
-            message.text && (
-              <li
-                key={index}
-                className={`my-4 p-4 rounded-lg max-w-xs transition-all duration-200 ease-in-out ${
-                  message.type === 'user'
-                    ? 'bg-blue-200 text-gray-700 ml-auto rounded-br-none'
-                    : 'bg-gray-200 text-gray-700 mr-auto rounded-bl-none'
-                } `}>
-                {message.text}
-              </li>
-            )
-        )}
-        <div ref={messagesEndRef} />
-      </ul>
-
-      <form
-        onSubmit={handleSubmit}
-        className='flex items-center justify-between bg-white rounded-lg shadow-md px-6 py-3 max-w-lg w-full'>
-        <input
-          type='text'
-          value={inputValue}
-          onChange={handleInputChange}
-          className='flex-grow bg-gray-200 rounded-md px-4 py-2 mr-4 text-lg focus:outline-none focus:ring-2 focus:ring-indigo-400'
-          placeholder='Type a message'
-        />
-        <button
-          type='submit'
-          className='text-white bg-indigo-600 px-6 py-2 rounded-md hover:bg-indigo-700 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-indigo-400'>
-          Send
-        </button>
-      </form>
+    <div className='h-screen bg-gray-100 flex items-center justify-center'>
+      <div className='w-full max-w-md bg-white rounded overflow-hidden shadow-lg py-4'>
+        <div className='px-6 pb-4 pt-2'>
+          <div className='font-bold text-xl mb-2 text-center'>
+            Welcome to BCX Chat
+          </div>
+          <p className='text-gray-700 text-base text-center'>
+            Please select your role to proceed.
+          </p>
+        </div>
+        <div className='px-6 pt-4 pb-2'>
+          <button
+            onClick={() => router.push('/agent/login')}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-4 rounded w-full transition duration-200'>
+            Login as Agent
+          </button>
+          <button
+            onClick={() => router.push('/user/login')}
+            className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full transition duration-200'>
+            Login as User
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ChatPage;
+export default Index;
