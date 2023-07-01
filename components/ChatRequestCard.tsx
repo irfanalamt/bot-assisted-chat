0ec348ebd1191b7e68b290e752cc2370dd17267f
Irfan <irfanalamt@gmail.com>
2023-07-01 15:50:01 -0400

    Refactor: adjust max-width; convert component to tsx

---
+++ components/ChatRequestCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+interface ChatRequestCardProps {
+  userName?: string;
+  handleDecline: () => void;
+  handleAccept: () => void;
+}
+
+const ChatRequestCard: React.FC<ChatRequestCardProps> = ({
+  userName = 'UserABC',
+  handleDecline,
+  handleAccept,
+}) => {
+  return (
+    <div className='flex flex-col max-w-max bg-white shadow-lg rounded-lg p-6 space-y-4'>
+      <div className='flex justify-between items-center'>
+        <h2 className='text-lg font-medium text-gray-700'>
+          <span className='text-blue-600'>{userName}</span> has sent a
+          connection request.
+        </h2>
+      </div>
+
+      <div className='flex'>
+        <button
+          onClick={handleAccept}
+          className='text-white bg-green-500 px-6 py-2 rounded-md hover:bg-green-600 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-green-400'>
+          Accept
+        </button>
+        <button
+          onClick={handleDecline}
+          className='text-white bg-red-500 px-6 py-2 rounded-md hover:bg-red-600 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-400 ml-2'>
+          Decline
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatRequestCard;
