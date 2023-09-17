0d1075e69efa0b175ca4ee2f6f5e1393355c6284
Irfan <irfanalamt@gmail.com>
2023-09-17 19:02:32 -0400

    Refactor: extract confirm action modal into a separate component

--- components/ManageAdmins.jsx
+++ components/ManageAdmins.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
+import ConfirmActionModal from './ConfirmActionModal';
 
 function ManageAdmins({userDetails, goToHome}) {
   const [admins, setAdmins] = useState([]);
@@ -332,49 +333,12 @@ function ManageAdmins({userDetails, goToHome}) {
             ))}
           </div>
           {deleteAdminId && (
-            <div className='fixed z-10 inset-0 overflow-y-auto'>
-              <div className='flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
-                <div className='fixed inset-0 transition-opacity'>
-                  <div className='absolute inset-0 bg-gray-500 opacity-75'></div>
-                </div>
-
-                <span className='hidden sm:inline-block sm:align-middle sm:h-screen'>
-                  &#8203;
-                </span>
-
-                <div className='inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'>
-                  <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
-                    <div className='sm:flex sm:items-start'>
-                      <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
-                        <h3 className='text-lg leading-6 font-medium text-gray-900'>
-                          Confirm Delete
-                        </h3>
-                        <div className='mt-2'>
-                          <p className='text-sm text-gray-500'>
-                            Are you sure you want to delete this admin? This
-                            action cannot be undone.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
-                    <button
-                      type='button'
-                      className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm'
-                      onClick={confirmDeleteAdmin}>
-                      Delete
-                    </button>
-                    <button
-                      type='button'
-                      className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
-                      onClick={() => setDeleteAdminId(null)}>
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ConfirmActionModal
+              action='delete'
+              actionText='Are you sure you want to delete this admin? This action cannot be undone.'
+              handleYes={confirmDeleteAdmin}
+              handleNo={() => setDeleteAdminId(null)}
+            />
           )}
         </>
       )}
